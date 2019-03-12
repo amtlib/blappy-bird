487d4975580125017a76abf73ee5f2032b296871
amtlib <grzegorz.andrzej.pach@gmail.com>
2019-03-12 22:59:39 +0100

    Reset settings instead of location.reload()

--- js/main.js
+++ js/main.js
@@ -17,14 +17,18 @@ function draw() {
     bird.update();
     
     pipes.map(pipe => pipe.update());
+    
     let oldLen = pipes.length
     pipes = pipes.filter(pipe => !pipe.offscreen());
     if(oldLen > pipes.length) pipes.push(new Pipe(520));
     
-    bird.draw();
-    if(bird.dead()) location.reload();
     pipes.map(pipe => pipe.draw());
-    console.log(pipes.length)
+
+    bird.draw();
+    if(bird.dead()){
+        pipes = [];
+        setup()
+    }
 }
 function keyPressed() {
     bird.up();
